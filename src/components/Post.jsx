d6751738f91b265420bdc46766a416c12f6e500e
ajay <ajaymevda999@gmail.com>
2023-09-30 15:27:52 +0530

    Custom Components

---
+++ src/components/Post.jsx
@@ -0,0 +1,12 @@
+import classes from "./Post.module.css"
+function Post(props) {
+    const {author, body} = props;
+    return (
+        <div className={classes.post}>
+            <p className={classes.author}>{author}</p>
+            <p className={classes.text}>{body}</p>
+        </div>
+    )
+}
+
+export default Post;
---
+++ src/components/PostList.jsx
@@ -0,0 +1,14 @@
+import Post from "./Post";
+
+function PostList() {
+    return(
+        <>
+            <Post author="Leela" body="React is an awesome course!" />
+            <Post author="Pravalika" body="React is an awesome course!" />
+            <Post author="Bharath" body="React is an awesome course!" />
+            <Post author="Prasad" body="React is an awesome course!" />
+        </>
+    )
+}
+
+export default PostList;
